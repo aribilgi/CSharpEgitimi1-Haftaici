85e0a186bf6525c5e19eb098b9dd376d3851f466
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2021-11-25 21:30:28 +0300

    25-11-2021

--- WindowsFormsAppAdoNet/Form1.cs
+++ WindowsFormsAppAdoNet/Form1.cs
@@ -16,15 +16,24 @@ namespace WindowsFormsAppAdoNet
         {
             InitializeComponent();
         }
-
+        ProductDal productDal = new ProductDal();
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            DGVUrunListesi.DataSource = productDal.GetAllDataTable(); //GetAll();
         }
 
         private void BtnKaydet_Click(object sender, EventArgs e)
         {
-
+            productDal.Add(
+                new Product
+                {
+                    UrunAdi = TxtUrunAdi.Text,
+                    StokMiktari = Convert.ToInt32(TxtStokMiktari.Text),
+                    UrunFiyati = Convert.ToDecimal(TxtUrunFiyati.Text)
+                }
+                );
+            DGVUrunListesi.DataSource = productDal.GetAll();
+            MessageBox.Show("Ürün Başarıyla Eklendi");
         }
     }
 }
--- WindowsFormsAppAdoNet/ProductDal.cs
+++ WindowsFormsAppAdoNet/ProductDal.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace WindowsFormsAppAdoNet
 {
@@ -11,7 +12,62 @@ namespace WindowsFormsAppAdoNet
     {
         //Veritabanı işlemleri için AdoNet kütüphanesini kullanıyoruz
         //Ado neti kullanmak için yukarıya using kısmına system.data.sqlclient i eklemeliyiz
-        SqlConnection _connection = new SqlConnection(); //Sql server a bağlanma nesnemiz
+        SqlConnection _connection = new SqlConnection(@"server=(LocalDB)\MSSQLLocalDB; initial catalog=UrunYonetimiAdoNet; Integrated security=True"); //Sql server a bağlanma nesnemiz
+        void ConnectionKontrol()//Veritabanı bağlantısının açık olup olmadığını kontrol eden metot
+        {
+            if (_connection.State == ConnectionState.Closed) //Yukardaki _connection bağlantı nesnemizin durumu bağlantı durumlarından kapalıya eşitse
+            {
+                _connection.Open(); // Bağlantıyı aç
+            }
+        }
+
+        public List<Product> GetAll()
+        {
+            ConnectionKontrol();
+            SqlCommand command = new SqlCommand("Select * from Products", _connection);
+            SqlDataReader reader = command.ExecuteReader();
+            List<Product> products = new List<Product>();
+
+            while (reader.Read())
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    UrunAdi = reader["UrunAdi"].ToString(),
+                    StokMiktari = Convert.ToInt32(reader["StokMiktari"]),
+                    UrunFiyati = Convert.ToDecimal(reader["UrunFiyati"])
+                };
+                products.Add(product);
+            }
+            reader.Close();
+            _connection.Close();
+
+            return products;
+        }
+
+        public DataTable GetAllDataTable()
+        {
+            ConnectionKontrol();//Bağlantı kontrolü yaptık
+            SqlCommand komut = new SqlCommand("select * from products", _connection);//veritabanına sorgumuzu gönderdik
+            SqlDataReader reader = komut.ExecuteReader();//veritabanındaki kayıtları okuduk
+            DataTable dataTable = new DataTable();//Okuduğumuz kayıtları yükleyeceğimiz data tabloyu oluşturduk
+            dataTable.Load(reader);//data tabloya veritabanından çektiğimiz kayıtları yükledik
+            reader.Close();//veri okuyucuyu kapattık
+            _connection.Close();
+            return dataTable;//kayıtların yüklendiği data tabloyu geriye döndürdük
+        }
+
+        public void Add(Product product)
+        {
+            ConnectionKontrol();
+            SqlCommand command = new SqlCommand("Insert into Products values (@UrunAdi,@UrunFiyati,@StokMiktari)", _connection);
+            command.Parameters.AddWithValue("@UrunAdi", product.UrunAdi);
+            command.Parameters.AddWithValue("@UrunFiyati", product.UrunFiyati);
+            command.Parameters.AddWithValue("@StokMiktari", product.StokMiktari);
+            command.ExecuteNonQuery();
+            command.Dispose();
+            _connection.Close();
+        }
 
     }
 }
