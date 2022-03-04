b4c23f6cfa9d0467039a52ad59c22b5725be0c89
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-03-04 21:26:21 +0300

    4-3-2022 Mvc Models

---
+++ MVCEgitim/Controllers/Mvc08ViewToControllerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEgitim.Controllers
+{
+    public class Mvc08ViewToControllerController : Controller
+    {
+        // GET: Mvc08ViewToController
+        public ActionResult Index(string txtAra) // Mvc de ön yüzle eşleşmeler bu şekilde textbox ile aynı isimde parametre kullanılarak yapılıyor
+        {
+            ViewBag.GetVerisi = "Aranan kelime : " + txtAra;
+            return View();
+        }
+        [HttpPost] // Aşağıdaki Index action unun türünün post metodu olduğunu ayarladık
+        public ActionResult Index(string text1, string liste, bool cbOnay) // Mvc de ön yüzdeki elemanların name alanıyla eşleşme yapılır Id ile değil!
+        {
+            //1. yöntem parametrelerden gelen veri;
+            ViewBag.Mesaj = "textboxdan gelen veri : " + text1;
+            ViewData["Vdata"] = "checkboxdan gelen veri : " + cbOnay;
+            TempData["Tdata"] = "listboxdan gelen veri : " + liste;
+
+            //2. yöntem Request.Form ile veri alma
+            var txt = Request.Form["text1"];
+            var ddl = Request.Form["liste"];
+            //var chb = Request.Form["cbOnay"]; true,false dönüyor
+            var chb = Request.Form.GetValues("cbOnay")[0];
+
+            ViewBag.Mesaj2 = "textboxdan gelen veri : " + txt;
+            ViewData["Vdata2"] = "checkboxdan gelen veri : " + chb;
+            TempData["Tdata2"] = "listboxdan gelen veri : " + ddl;
+
+            return View();
+        }
+    }
+}
---
+++ MVCEgitim/Controllers/Mvc09ModelBindingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEgitim.Controllers
+{
+    public class Mvc09ModelBindingController : Controller
+    {
+        // GET: Mvc09ModelBinding
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Kullanici()
+        {
+            return View();
+        }
+    }
+}
