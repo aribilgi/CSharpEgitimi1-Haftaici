229c602fefc0ef91a086c13d447dbbd40048b4dd
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-03-15 19:31:44 +0300

    15-3-2022 Bugra

---
+++ MVCEgitim/Controllers/Mvc10ModelValidationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEgitim.Controllers
+{
+    public class Mvc10ModelValidationController : Controller
+    {
+        // GET: Mvc10ModelValidation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCEgitim/Models/Uye.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCEgitim.Models
+{
+    public class Uye
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+        public string TCKimlikNo { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public string KullaniciAdi { get; set; }
+    }
+}
