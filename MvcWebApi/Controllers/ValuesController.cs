8913571a1cf6d1b35dee272e4e200b6efca559ce
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-05-06 21:24:32 +0300

    6-5-22 Mvc web api

---
+++ MvcWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcWebApi.Controllers
+{
+    public class ValuesController : ApiController // BU controller controller class ı yerine ApiController classından miras alır böyece normal controller dan ayrılıp api özellikleri taşımaya başlar
+    {
+        // GET api/values : adres çubuğunda proje isminden sonra api/values yazılırsa api çalışır
+        public IEnumerable<string> Get() // Get metodu ile string listesi döndürüyor
+        {
+            return new string[] { "Klavye", "Monitör", "Mouse", "Kasa", "Kulaklık" }; // Geriye string dizisi döner
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "Mikrofon";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value) // Post metodu db ye kayıt eklemek için
+        {
+            // Burada api ye gelen post isteği sonucu db ye kayıt eklenir
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+            // Gelen Put isteği ile db deki kayıt burada güncellenir
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            // Gelen delete isteği ile id ile eşleşen kayıt db den silinir
+        }
+    }
+}
---
+++ MvcWebApi/Models/UrunDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MvcWebApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcWebApi.Models
+{
+    public class UrunDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
